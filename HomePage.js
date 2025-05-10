f070c4c2c6e6ef53455fc9fffdb1d95736d5d1aa
Arti-98 <chamlearti95@gmail.com>
2025-05-10 17:52:53 +0530

    Add files via upload

---
+++ HomePage.js
@@ -0,0 +1,29 @@
+// cypress/support/pages/HomePage.js
+
+class HomePage {
+  visit() {
+    cy.visit('https://demowebshop.tricentis.com/');
+  }
+
+  getHeaderLink(text) {
+    return cy.get('div.header-menu').contains(text);
+  }
+
+  getFooterLink(text) {
+    return cy.get('div.footer-block').contains(text);
+  }
+
+  search(keyword) {
+    cy.get('#small-searchterms').clear().type(`${keyword}{enter}`);
+  }
+
+  getSearchResults() {
+    return cy.get('.product-item');
+  }
+
+  getNoResultsMessage() {
+    return cy.contains('No products were found');
+  }
+}
+
+export default HomePage;
---
+++ productDetails.cy.js
@@ -0,0 +1,28 @@
+import ProductDetailsPage from './support/pages/ProductDetailsPage';
+
+const productDetailsPage = new ProductDetailsPage();
+
+describe('Product Details Verification', () => {
+  beforeEach(() => {
+    // Example product URL: "Build your own computer"
+    productDetailsPage.visitProductPage('https://demowebshop.tricentis.com/build-your-own-computer');
+  });
+
+  it('Verifies product title, description, and price', () => {
+    productDetailsPage.getProductTitle().should('contain.text', 'Build your own computer');
+    productDetailsPage.getProductDescription().should('be.visible');
+    productDetailsPage.getProductPrice().should('match', /\$\d+/);
+  });
+
+  it('Verifies specifications or specs section if available', () => {
+    productDetailsPage.getProductSpecs().should('exist');
+  });
+
+  it('Ensures product image is loaded and visible', () => {
+    productDetailsPage.getProductImage()
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
